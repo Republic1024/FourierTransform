bf8c4ecd1613e8777dc984cef22b22a2e4f96043
Republic <g8714y@outlook.com>
2024-09-29 17:31:44 +0800

    Initial commit or merge or rebase remote changes

--- run.bat
+++ run.bat
@@ -1,29 +1,29 @@
-:: ��ȡ��ǰ�ļ�����
+:: 获取当前文件夹名
 for %%I in ("%%cd%%") do set "folder_name=FourierTransform"
 
-:: ʹ�õ�ǰ�ļ���������һ���µ� GitHub �ֿ�
+:: 使用当前文件夹名创建一个新的 GitHub 仓库
 gh repo create "FourierTransform" --public
 
-:: ��ʼ��һ���µ� Git �ֿ�
+:: 初始化一个新的 Git 仓库
 git init
 
-:: ��Զ�ֿ̲����Ϊ origin
+:: 将远程仓库添加为 origin
 git remote add origin https://github.com/Republic1024/FourierTransform.git
 
-:: ��Զ�ֿ̲�� main ��֧��ȡ���´���
+:: 从远程仓库的 main 分支拉取最新代码
 git pull origin main
 
-:: �Ƴ�δ���ٵ��ļ������� .DS_Store��Mac ϵͳ���ɵ������ļ���
+:: 移除未跟踪的文件，例如 .DS_Store（Mac 系统生成的隐藏文件）
 del /F /Q .DS_Store
 
-:: �����и�����ӵ��ݴ������ų� .pth �ļ�
+:: 将所有更改添加到暂存区，排除 .pth 文件
 git add .
 
-:: �ύ�ݴ����ĸ��Ĳ�����ύ��Ϣ
-git commit -m "This is new changes"
+:: 提交暂存区的更改并添加提交信息
+git commit -m "Initial commit or merge or rebase remote changes"
 
-:: �����ص� main ��֧�����Զ�� main ��֧�������ύ
+:: 将本地的 main 分支变基到远程 main 分支的最新提交
 git branch -M main
 
-:: ������ main ��֧�ĸ������͵�Զ�� main ��֧
+:: 将本地 main 分支的更改推送到远程 main 分支
 git push -u origin main
